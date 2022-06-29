7d1af625660c3174b35aaa029ebd0905f8fe281d
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2022-06-29 15:52:57 -0700

    card action area, faq background color

--- src/FaqCard.js
+++ src/FaqCard.js
@@ -1,6 +1,6 @@
 
 import {
-  Card, Collapse, Box,
+  Card, CardActionArea, Collapse, Box,
   Typography,
 } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
@@ -15,28 +15,31 @@ export default function FaqCard(props) {
     <>
       <Card
         sx={{
+          background: '#ffffff44',
           cursor: 'pointer'
         }}
         onClick={() => setDropDown((prevState) => !prevState)}
       >
-        <Box display="flex" flexDirection="row" sx={{ p: 2 }}>
-          <Typography color="secondary">{props.question}</Typography>
-          <Box marginLeft="auto" paddingRight={2}>
-            {/* {dropDown ? <ExpandLess /> : <ExpandMore />} */}
-            <ExpandLess
-              sx={{
-                transition: 'transform 0.5s ease',
-                // transform: dropDown ? "scaleY(-1)" : "scaleY(1)",
-                transform: dropDown ? "rotate(0deg)" : "rotate(180deg)",
-              }}
-            />
+        <CardActionArea>
+          <Box display="flex" flexDirection="row" sx={{ p: 2 }}>
+            <Typography color="secondary">{props.question}</Typography>
+            <Box marginLeft="auto" paddingRight={2}>
+              {/* {dropDown ? <ExpandLess /> : <ExpandMore />} */}
+              <ExpandLess
+                sx={{
+                  transition: 'transform 0.5s ease',
+                  // transform: dropDown ? "scaleY(-1)" : "scaleY(1)",
+                  transform: dropDown ? "rotate(0deg)" : "rotate(180deg)",
+                }}
+              />
+            </Box>
           </Box>
-        </Box>
-        <Collapse in={dropDown}>
-          <Box sx={{ p: 2, pt: 0 }}>
-            <Typography>{props.answer}</Typography>
-          </Box>
-        </Collapse>
+          <Collapse in={dropDown}>
+            <Box sx={{ p: 2, pt: 0 }}>
+              <Typography>{props.answer}</Typography>
+            </Box>
+          </Collapse>
+        </CardActionArea>
       </Card>
 
     </>
