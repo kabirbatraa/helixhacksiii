c302099be1a78674054f0b03e809caf413681b61
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2022-08-23 20:12:58 -0700

    disabled links to join and added devpost link

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,14 @@
 
 import {
-  Box, Typography, Button, 
+  Box, Typography, Button,
 } from '@mui/material';
 
 
 export default function Header(props) {
   return (
-    <Box display="flex" flexDirection="column" justifyContent="center" 
+    <Box display="flex" flexDirection="column" justifyContent="center"
       sx={{
-        height: "60vh", 
+        height: "60vh",
         // background: "#00000011"
       }}
     >
@@ -24,22 +24,36 @@ export default function Header(props) {
       </Box>
 
       <Box display="flex" justifyContent="center" >
-        <Button variant="contained" sx={{m: 1, fontSize: "1em"}} color="secondary" onClick={() => window.open("https://forms.gle/uMecarmWPtMeLJpF6")}>
+        <Button variant="contained" sx={{ m: 1, fontSize: "1em" }} color="secondary" onClick={() => window.open("https://forms.gle/uMecarmWPtMeLJpF6")} disabled>
           Participant
         </Button>
-        <Button variant="contained" sx={{m: 1, fontSize: "1em"}} color="secondary" onClick={() => window.open("https://forms.gle/idLXU9ikzgWv581C7")}>
+        <Button variant="contained" sx={{ m: 1, fontSize: "1em" }} color="secondary" onClick={() => window.open("https://forms.gle/idLXU9ikzgWv581C7")} disabled>
           Mentor
         </Button>
-        <Button variant="contained" sx={{m: 1, fontSize: "1em"}} color="secondary" onClick={() => window.open("https://forms.gle/BxH4RGrNnPxZF4dh6")}> 
+        <Button variant="contained" sx={{ m: 1, fontSize: "1em" }} color="secondary" onClick={() => window.open("https://forms.gle/BxH4RGrNnPxZF4dh6")} disabled>
           Judge
         </Button>
       </Box>
-      
+
       <Box display="flex" justifyContent="center">
-      <Button variant="outlined" color="secondary" sx={{mt: 1, fontSize: "1em"}} onClick={() => window.open("https://discord.gg/UJCKSH9SvP")}>
-        Join our Discord
-      </Button>
+        <Button variant="contained" color="secondary" sx={{ mt: 1, fontSize: "1em" }} onClick={() => window.open("https://discord.gg/UJCKSH9SvP")} disabled>
+          Join our Discord
+        </Button>
       </Box>
+
+      <Box textAlign='center' sx={{ m: 2 }}>
+        <Typography variant='h4'>This hackathon has ended!</Typography>
+        <Typography variant='h5'>
+          See winners on
+          <Button variant="text" color="secondary" sx={{ fontSize: "1em" }} onClick={() => window.open("https://helix-hacks-iii.devpost.com/")} >
+            Devpost
+          </Button>
+        </Typography>
+
+
+
+      </Box>
+
 
     </Box>
   )
