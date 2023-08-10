9563bf89070f78a318e2870cb9eebe73db808ab1
Kabir Batra <89661761+kabirbatraa@users.noreply.github.com>
2023-08-09 17:47:11 -0700

    attempt to run website using vite

---
+++ vite-test/src/PersonCard.jsx
@@ -0,0 +1,40 @@
+
+import {
+  Typography, Card, CardMedia, CardContent,
+} from '@mui/material';
+
+export default function PersonCard(props) {
+
+  // props.
+  
+  // imageUrl
+  // name
+  // position
+
+  // url
+  // name
+  // mentor/judge
+  
+  return <>
+    <Card sx={{background: "#ffffff22"}}>
+      <CardMedia
+        component="img"
+        height="150"
+        image={props.imageUrl}
+        alt={props.name}
+        sx={{
+          objectPosition: "center center"
+        }}
+      />
+      {/* fixed height might look best (weird to have different offset heights) */}
+      <CardContent sx={{height: "85px"}}> 
+        <Typography gutterBottom variant="h6" component="div" sx={{lineHeight: "1.3em"}}>
+          {props.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          {props.position}
+        </Typography>
+      </CardContent>
+    </Card>
+  </>
+}
